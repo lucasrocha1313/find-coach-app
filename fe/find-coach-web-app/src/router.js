bafc5054c6a6858d2a1d928b531958ec47b41558
Lucas Rocha <a.lucas.a.rocha@gmail.com>
2023-01-07 13:36:40 -0300

    add template to detail component

--- fe/find-coach-web-app/src/router.js
+++ fe/find-coach-web-app/src/router.js
@@ -11,9 +11,14 @@ const router = createRouter({
     routes: [
         {path: '/', redirect: '/coaches'},
         {path: '/coaches', component: CoachesList},
-        {path: '/coaches/:id', component: CoachDetail, children: [
+        {
+            path: '/coaches/:id',
+            component: CoachDetail,
+            props: true,
+            children: [
                 {path: 'contact', component: ContactCoach}
-            ]},
+            ]
+        },
         {path: '/register', component: CoachRegistration},
         {path: '/requests', component: RequestReceived},
         {path: '/:notFound(.*)', component: NotFound}
