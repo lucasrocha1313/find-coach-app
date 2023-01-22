66b2a147e7f355a3f8cae430717ae05cab574034
Lucas Rocha <a.lucas.a.rocha@gmail.com>
2023-01-22 17:04:12 -0300

    send and show requests to coaches

--- fe/find-coach-web-app/src/pages/requests/ContactCoach.vue
+++ fe/find-coach-web-app/src/pages/requests/ContactCoach.vue
@@ -1,12 +1,102 @@
 <template>
-  <div>Contact coach</div>
+  <form @submit.prevent="submitForm">
+    <div class="form-control">
+      <label for="email">Your E-Mail</label>
+      <input type="email" id="email" v-model.trim="email">
+    </div>
+    <div class="form-control">
+      <label for="message">Message</label>
+      <textarea rows="5" id="message" v-model.trim="message"></textarea>
+    </div>
+    <p v-if="!formIsValid" class="errors">Please enter a valid email and a non-empty message</p>
+    <div class="actions">
+      <base-button>Send Message</base-button>
+    </div>
+  </form>
 </template>
 <script>
+import BaseButton from "@/components/ui/BaseButton.vue";
+
 export default {
-  name: "ContactCoach"
+  name: "ContactCoach",
+  components: {BaseButton},
+  data() {
+    return {
+      email: '',
+      message: '',
+      formIsValid: true
+    }
+  },
+  methods: {
+    submitForm() {
+      this.formIsValid = true
+      if(!this.isValidEmail || this.message === '') {
+        this.formIsValid = false
+        return
+      }
+
+      this.$store.dispatch('requests/contactCoach', {
+        email: this.email,
+        message: this.message,
+        coachId: this.$route.params.id
+      })
+      this.$router.replace('/coaches')
+    }
+
+  },
+  computed: {
+    isValidEmail() {
+      return String(this.email)
+          .toLowerCase()
+          .match(
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          )
+    }
+  }
 }
 </script>
 
 <style scoped>
+form {
+  margin: 1rem;
+  border: 1px solid #ccc;
+  border-radius: 12px;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+
+.errors {
+  font-weight: bold;
+  color: red;
+}
+
+.actions {
+  text-align: center;
+}
 
 </style>
--- fe/find-coach-web-app/src/pages/requests/RequestReceived.vue
+++ fe/find-coach-web-app/src/pages/requests/RequestReceived.vue
@@ -1,13 +1,46 @@
 <template>
- <div>Requests</div>
+ <section>
+   <base-card>
+     <header>
+       <h2>Request received</h2>
+     </header>
+     <ul v-if="hasRequests">
+       <request-item v-for="req in receivedRequests" :key="req.id" :email="req.userEmail" :message="req.message"/>
+     </ul>
+     <h3 v-else>You haven't received any request yet!</h3>
+   </base-card>
+ </section>
 </template>
 
 <script>
+import BaseCard from "@/components/ui/BaseCard.vue";
+import RequestItem from "@/components/requests/RequestItem.vue";
+
 export default {
-  name: "RequestReceived"
+  name: "RequestReceived",
+  components: {RequestItem, BaseCard},
+  computed: {
+    receivedRequests() {
+      return this.$store.getters['requests/requests']
+    },
+    hasRequests() {
+      return this.$store.getters['requests/hasRequests']
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+header {
+  text-align: center;
+}
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+h3 {
+  text-align: center;
+}
 </style>
