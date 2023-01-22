be600570840a3c8f6bf83daf57b6716097950737
Lucas Rocha <a.lucas.a.rocha@gmail.com>
2023-01-22 17:09:28 -0300

    filter requests by logged user id

--- fe/find-coach-web-app/src/store/modules/requests/index.js
+++ fe/find-coach-web-app/src/store/modules/requests/index.js
@@ -22,11 +22,11 @@ export default {
         }
     },
     getters: {
-        requests(state) {
-            return state.requests
+        requests(state, _, _2, rootGetters) {
+            return state.requests.filter( req => req.coachId === rootGetters.userId)
         },
-        hasRequests(state) {
-            return state.requests && state.requests.length > 0
+        hasRequests(_, getters) {
+            return getters.requests && getters.requests.length > 0
         }
     }
 }
