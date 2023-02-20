8c48e53e414fc31419a0faf45b85880fc5d35e3e
Lucas Rocha <a.lucas.a.rocha@gmail.com>
2023-02-20 08:16:33 -0300

    not found page

--- fe/find-coach-web-app/src/pages/NotFound.vue
+++ fe/find-coach-web-app/src/pages/NotFound.vue
@@ -1,7 +1,17 @@
-
+<template>
+  <section>
+    <base-card>
+      <h2>Page not found</h2>
+      <p>This page could not be found - maybe check out all our <router-link to="/coaches">coaches</router-link></p>
+    </base-card>
+  </section>
+</template>
 <script>
+import BaseCard from "@/components/ui/BaseCard.vue";
+
 export default {
-  name: "NotFound"
+  name: "NotFound",
+  components: {BaseCard}
 }
 </script>
 
